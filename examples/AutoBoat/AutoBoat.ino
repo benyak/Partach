b47cb301440d558d5b5ba6c7fdda7f1749076896
nirdrang <33868542+nirdrang@users.noreply.github.com>
2017-12-13 15:30:05 +0200

    change the way to read SMS

--- examples/AutoBoat/AutoBoat.ino
+++ examples/AutoBoat/AutoBoat.ino
@@ -46,6 +46,8 @@ Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 //Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
 
 uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+boolean readSMS(char* sms_buffer);
+
 uint8_t type;
 
 void setup() 
@@ -111,83 +113,18 @@ void setup()
   compass.m_max = (LSM303::vector<int16_t>){+572,   +331,   +553};
 }
 
-char fonaNotificationBuffer[64];          //for notifications from the FONA
 char smsBuffer[250];
 
 void loop() 
 {
-/***************************************************************************************************/
-  char* bufPtr = fonaNotificationBuffer;    //handy buffer pointer
-  
-  if (fona.available())      //any data available from the FONA?
+
+  //look for a new command
+  if(readSMS(smsBuffer) == true)
   {
-    int slot = 0;            //this will be the slot number of the SMS
-    int charCount = 0;
-    //Read the notification into fonaInBuffer
-    do  
-    {
-      *bufPtr = fona.read();
-      Serial.write(*bufPtr);
-      delay(1);
-    } 
-    while ((*bufPtr++ != '\n') && (fona.available()) && (++charCount < (sizeof(fonaNotificationBuffer)-1)));
-    
-    //Add a terminal NULL to the notification string
-    *bufPtr = 0;
-
-    //Scan the notification string for an SMS received notification.
-    //  If it's an SMS message, we'll get the slot number in 'slot'
-    if (1 == sscanf(fonaNotificationBuffer, "+CMTI: " FONA_PREF_SMS_STORAGE ",%d", &slot)) 
-    {
-      Serial.println("******* SMS Code ******");
-      
-      Serial.print("slot: "); 
-      Serial.println(slot);
-      
-      char callerIDbuffer[32];  //we'll store the SMS sender number in here
-      
-      // Retrieve SMS sender address/phone number.
-      if (! fona.getSMSSender(slot, callerIDbuffer, 31)) 
-      {
-        Serial.println("Didn't find SMS message in slot!");
-      }
-      Serial.print(F("FROM: ")); Serial.println(callerIDbuffer);
-
-        // Retrieve SMS value.
-        uint16_t smslen;
-        if (fona.readSMS(slot, smsBuffer, 250, &smslen)) 
-        { // pass in buffer and max len!
-          Serial.println(smsBuffer);
-        }
-/*
-      //Send back an automatic response
-      Serial.println("Sending reponse...");
-      if (!fona.sendSMS(callerIDbuffer, "Hey, I got your text!")) 
-      {
-        Serial.println(F("Failed"));
-      } else 
-      {
-        Serial.println(F("Sent!"));
-      }
-*/
-      
-      // delete the original msg after it is processed
-      //   otherwise, we will fill up all the slots
-      //   and then we won't be able to receive SMS anymore
-      if (fona.deleteSMS(slot)) 
-      {
-        Serial.println(F("OK!"));
-      } 
-      else 
-      {
-        Serial.print(F("Couldn't delete SMS in slot ")); Serial.println(slot);
-        fona.print(F("AT+CMGD=?\r\n"));
-      }
-    }
+      //parse new command
+      Serial.println("Got command");
+      //execute command code ...
   }
-
-
-
 
  /*********************************************************************************************/
   
@@ -216,6 +153,7 @@ void loop()
   else 
   {
     Serial.println("Waiting for FONA GPS 3D fix...");
+    delay(1000);
 
     return;
   }
@@ -223,7 +161,7 @@ void loop()
   //Get the compass heading
   compass.read();
   
-  /*
+  /* 
   When given no arguments, the heading() function returns the angular
   difference in the horizontal plane between a default vector and
   north, in degrees.
@@ -394,4 +332,45 @@ void ComputeDestPoint(double lat1, double lon1, int iBear, int iDist, double *la
 void turnTo(int degrees)
 {
 
+}
+
+
+boolean readSMS(char* sms_buffer)
+{
+    
+    //look for sms message on slot #1, and delete  
+    //read the first slot SMS
+    uint16_t smslen;
+    if (fona.readSMS(1, sms_buffer, 250, &smslen)) 
+    { 
+      // pass in buffer and max len!
+      Serial.println(F("********* Got an SMS message ******* "));
+      Serial.println(smsBuffer);
+      char callerIDbuffer[32];  //we'll store the SMS sender number in here
+      
+      // Retrieve SMS sender address/phone number.
+      if (! fona.getSMSSender(1, callerIDbuffer, 31)) 
+      {
+        Serial.println("Didn't find SMS message!");
+      }
+      else
+      {
+        Serial.print(F("FROM: ")); Serial.println(callerIDbuffer);
+      }
+
+      //delete the message
+      if (fona.deleteSMS(1)) 
+      {
+        Serial.println(F("Delete OK!"));
+      } 
+      else 
+      {
+        Serial.println(F("Couldn't delete SMS")); 
+      }
+
+      return true;
+    }
+
+    //no messages
+    return false;
 }
