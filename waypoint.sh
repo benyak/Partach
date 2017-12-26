e2ef9d5be9134ee57dc5827d53a973acacf0ca4c
nirdrang <33868542+nirdrang@users.noreply.github.com>
2017-12-26 14:00:26 +0200

    .

--- waypoint.sh
+++ waypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Latitude,Longitude,Icon
-grep Waypoint: $1 | awk -v f=2 -v t=3 '{for(i=f;i<=t;i++) printf("%s,117\n",$i);}'
+grep Waypoint: $1 | awk -v f=3 -v t=4 '{for(i=f;i<=t;i++) if(i==t) printf("%s\n",$i);else printf("%s,",$i)}'
